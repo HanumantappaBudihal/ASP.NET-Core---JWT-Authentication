b40cb87219b53c504be7593cafd75c1dd18709e3
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-07-19 23:34:30 +0530

    Added the SQL script for supporting database and table creation (also inserted the init data)

---
+++ src/Hexagonal.JWTAuthentication/Hexagonal.RESTApiWithJWTSample/SQL Scripts/InitDatabaseAndTableCreation.sql
@@ -0,0 +1,28 @@
+﻿CREATE DATABASE Learning
+GO
+
+USE  Learning
+GO 
+
+CREATE TABLE Products(
+ProductId INT IDENTITY(1,1) PRIMARY KEY,
+Name VARCHAR(100) NOT NULL,
+Category VARCHAR(100),
+Color VARCHAR(20),
+UnitPrice DECIMAL NOT NULL,
+AvailableQuantity INT NOT NULL)
+GO
+
+CREATE TABLE Users(
+UserId INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(30) NOT NULL,
+UserName VARCHAR(30) NOT NULL,
+Email VARCHAR(50) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+CreatedDate DATETIME DEFAULT(GETDATE()) NOT NULL)
+GO
+
+INSERT INTO Users(FirstName, LastName, UserName, Email, Password) 
+VALUES ('Hanumantappa', 'Budihal', 'Admin', 'admin@abc.com', '$admin@2020')
+GO
